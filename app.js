1bf50d029b86010e9b829feaa651c277ac93b223
Ethan Choi <ethanchoi.812@gmail.com>
2017-01-31 12:05:32 +0800

    Added computer turn

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 
 //choose x or o
-var player='';
+var player=''; var complayer='';
 var playerX = document.getElementById('x-btn');
 var playerO = document.getElementById('o-btn');
 
@@ -9,6 +9,7 @@ var playerElements = document.getElementsByClassName('player-btn');
 for (var i=0; i<playerElements.length; i++){
 	playerElements[i].addEventListener('click', function(){
 		player = this.id;
+		player === 'x-btn' ? complayer = 'o-btn' : complayer = 'x-btn';
 	}, false);
 }
 
@@ -19,14 +20,33 @@ var playerObj = {
 }
 //click to display
 var boxElements = document.getElementsByClassName('box');
+var clickedBoxes = [];
+//random box is marked
+function randomNum() {
+  var a;
+  var min = Math.ceil(9);
+  var max = Math.floor(1);
+  
+  a = Math.floor(Math.random() * (max - min)) + min;
+  
+  return a;
+
+  }
 
 for (var j=0; j<boxElements.length; j++){
 	boxElements[j].addEventListener('click', function(){
 		this.innerHTML = '<p>' + playerObj[player] + '</p>';
+		clickedBoxes.push(this.id);
+		var randomBox = 'box' + randomNum();
+		//computer clicks random box that is not clicked
+		//set delay
+		if(clickedBoxes.indexOf(randomBox) === -1){
+		document.getElementById(randomBox).innerHTML = '<p>' + playerObj[complayer] + '</p>';
+		} 
 	}, false);
 }
 
-//take turns with com
+
 
 //3 in a row to win
-//rest on win
+//reset on win
