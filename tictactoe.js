579c3c5e0df67e0af9f6d0ba2f30f54b5f6aa445
Ethan Choi <ethanchoi.812@gmail.com>
2017-02-05 22:59:41 +0800

    Updated conditions and events for winning

--- tictactoe.js
+++ tictactoe.js
@@ -2,10 +2,9 @@
 var player = {
 	playerid: '',
 	complayerid: '',
-	playerclickedArr: [],
-	comclickedArr:[],
 }
 
+//used for checking later
 var boxObj = {
 	box0:'',
 	box1:'',
@@ -18,75 +17,103 @@ var boxObj = {
 	box8:'',
 }
 
-//assign x or o to player and the other to com
+//used for checking later
+var winning = [['box0', 'box1', 'box2'],['box3', 'box4', 'box5'],['box6', 'box7', 'box8'],['box0', 'box3', 'box6'],['box1', 'box4', 'box7'],['box2', 'box5', 'box8'],['box0', 'box4', 'box8'],['box2', 'box4', 'box6']];
+
+var alertme = document.getElementById('alert');
+
+//target buttons by class name
 var btnElements = document.getElementsByClassName('player-btn');
 
+//assign 'x' or 'o' to player on click and the other to com
 for (var i=0; i<btnElements.length; i++){
 	btnElements[i].addEventListener('click', function(){
+		
 		player.playerid = this.id;
 		player.playerid === 'x' ? player.complayerid = 'o' : player.complayerid = 'x';
-		console.log(player.playerid, player.complayerid);
+		
+		//clears alert msg
+		alertme.innerHTML = '';
+	
 	}, false);
-
 }
 
+//target boxes by class name
 var boxElements = document.getElementsByClassName('box');
-var boxclickedArr = [];
+
+//add click events to boxes
 
 for (var j=0; j<boxElements.length; j++){
 	boxElements[j].addEventListener('click', function (){
 	
-	var boxclicked = this.id;
+	//require player to select 'x' or 'o'
+	if (player.playerid === ''){
+		
+		alertme.innerHTML = '<p>Please select X or O</p>';
 	
-	//if box is not clicked on
-	if(boxclickedArr.indexOf(boxclicked) === -1){
+	} else {
+
+		//function playerOne(){
+
+	var boxclicked = this.id;
+
+	//box is marked X or O if box is not marked before
+	if(boxObj[boxclicked] === '') {
 	
 		//Box to get playerid on click 
-		document.getElementById(this.id).innerHTML = '<p>' + player.playerid + '</p>'; 
+		document.getElementById(this.id).innerHTML = '<p class="mark">' + player.playerid + '</p>'; 
+
+		//for checking later
 		boxObj[boxclicked] = player.playerid;
-		boxclickedArr.push(boxclicked);
-		player.playerclickedArr.push(boxclicked);
+	}
+			//check if player won yet?
+			winning.forEach(function(a){
+				if(boxObj[a[0]] === player.playerid && boxObj[a[1]] === player.playerid && boxObj[a[2]] === player.playerid){
+
+					//tells player of win
+					setTimeout(function(){
+						alertme.innerHTML = '<p>You Win!</p>';
+					}, 100);
+
+					
+					setTimeout(function(){
+						location.reload();
+					}, 1100);
+				}
+			});
 
 		//another box to get complayerid
 		var randomNum = function() {
 			var num = Math.floor(Math.random()*9);
-			return (boxclickedArr.indexOf('box'+ num) !== -1) ? randomNum() : num;
+			return boxObj['box' + num] !== '' ? randomNum() : num;
 		}
 
-		var randomBox = 'box' + randomNum();
+		//add delay
+		setTimeout(function(){
 
-		document.getElementById(randomBox).innerHTML = '<p>' + player.complayerid + '</p>';
-		boxObj[randomBox] = player.complayerid;
-		boxclickedArr.push(randomBox);
-		player.comclickedArr.push(randomBox);
+			var randomBox = 'box' + randomNum();
+
+			document.getElementById(randomBox).innerHTML = '<p class="mark">' + player.complayerid + '</p>';
 			
-		console.log(boxObj, boxclickedArr, player.playerclickedArr, player.comclickedArr);
-			//add delay
+			boxObj[randomBox] = player.complayerid;
+
+			winning.forEach(function(a){
+				if(boxObj[a[0]] === player.complayerid && boxObj[a[1]] === player.complayerid && boxObj[a[2]] === player.complayerid){
+					
+					setTimeout(function(){
+						alertme.innerHTML = '<p>Computer Wins!</p>';
+					}, 100);
+
+					}
+				});
+
+			},1200);
+
 			//block player if 2 winning boxes get filled
+			}
 		}
 	}, false); 
 }
 
-//winning conditions
-var winning = [
-['box0', 'box1', 'box2'],
-['box3', 'box4', 'box5'],
-['box6', 'box7', 'box8'],
-['box0', 'box3', 'box6'],
-['box1', 'box4', 'box7'],
-['box2', 'box5', 'box8'],
-['box0', 'box4', 'box8'],
-['box2', 'box4', 'box6']];
 
-for(var k=0; k<winning.length; k++){
-	for(var l=0; l<winning[k].length; l++)
-	if(boxObj[winning[k][l]] === 'x'){
-
-		} else
-		if(boxObj[winning[k][l]] === 'o'){
 			
-		}
-}
-
-//get box number by O and X
- 
